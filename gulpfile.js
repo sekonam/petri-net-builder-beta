859d30c1c0e9222bb013031bd7fc0895f4196093
sekonam <sekon.am@gmail.com>
2017-02-06 14:46:16 +0200

    less compile added

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
 
-gulp.task('build', function () {
+gulp.task('build-js', function () {
     return browserify({entries: './src/index.js', extensions: ['.js'], debug: true})
         .transform('babelify', {presets: ['es2015', 'react']})
         .bundle()
@@ -11,8 +13,15 @@ gulp.task('build', function () {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', ['build'], function () {
-    gulp.watch('*.js', ['build']);
+gulp.task('build-css', function () {
+  gulp.src([
+    './node_modules/bootstrap-less/index.less',
+    './node_modules/react-bootstrap-switch/src/less/bootstrap3/react-bootstrap-switch.less',
+    './node_modules/react-select/less/default.less',
+    './style.css',
+  ]).pipe(less())
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['build']);
+gulp.task('default', ['build-js', 'build-css',]);
